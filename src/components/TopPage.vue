6296e2faa85a6f20b29966788b5d03600f87b220
Sohei Uchino <suchino@jp.ibm.com>
2019-12-28 17:49:36 +0900

    文言修正

--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -4,14 +4,8 @@
       <section>
         <v-parallax src="@/assets/bg1.png" height="500">
           <v-layout column align-center justify-center class="white--text">
-            <img
-              src="@/assets/innovatty_logo.png"
-              alt="Vuetify.js"
-              height="200"
-            />
-            <h1 class="white--text mb-2 display-1 text-center">
-              innovators & developers community
-            </h1>
+            <img src="@/assets/innovatty_logo.png" alt="Vuetify.js" height="200" />
+            <h1 class="white--text mb-2 display-1 text-center">innovators & developers community</h1>
           </v-layout>
         </v-parallax>
       </section>
@@ -20,8 +14,8 @@
         <v-layout column wrap class="my-12" align-center>
           <v-flex xs12 sm4 class="my-4">
             <div class="text-center">
-              <h2 class="headline">The best way to start developing</h2>
-              <span class="subheading">Cras facilisis mi vitae nunc</span>
+              <h2 class="headline">Learn about this community's way.</h2>
+              <span class="subheading">Read follow contents and join Slack.</span>
             </div>
           </v-flex>
           <v-flex xs12>
@@ -35,18 +29,16 @@
       <section>
         <v-parallax src="@/assets/meetup_photo.png" height="380">
           <v-layout column align-center justify-center>
-            <div class="headline white--text mb-4 text-center">
-              Web development has never been easier
-            </div>
-            <em>Kick-start your application today</em>
+            <div class="headline white--text mb-4 text-center">Already a member?</div>
+            <em>If so, let's talk in Slack right away</em>
             <v-btn
               class="mt-12"
               color="blue lighten-2"
               dark
               large
-              href="/pre-made-themes"
-              >Get Started</v-btn
-            >
+              href="https://innovatty.slack.com/app_redirect?channel=_post"
+              target="_blank"
+            >Get Started</v-btn>
           </v-layout>
         </v-parallax>
       </section>
