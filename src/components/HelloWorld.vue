56a86e8211d8e675697fa0ffe10628d7ff205e21
Sohei Uchino <suchino@jp.ibm.com>
2019-12-28 14:24:51 +0900

    ロゴの追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,25 +2,13 @@
   <v-app light>
     <!-- <v-toolbar color="white">
       <v-toolbar-title v-text="title"></v-toolbar-title>
-    </v-toolbar> -->
+    </v-toolbar>-->
     <v-content>
       <section>
         <v-parallax src="@/assets/hero.jpeg" height="600">
           <v-layout column align-center justify-center class="white--text">
-            <img src="@/assets/vuetify.png" alt="Vuetify.js" height="200" />
-            <h1 class="white--text mb-2 display-1 text-center">
-              Parallax Template
-            </h1>
-            <div class="subheading mb-4 text-center">Powered by Vuetify</div>
-            <v-btn
-              class="mt-12"
-              color="blue lighten-2"
-              dark
-              large
-              href="/pre-made-themes"
-            >
-              Get Started
-            </v-btn>
+            <img src="@/assets/innovatty_logo.png" alt="Vuetify.js" height="200" />
+            <h1 class="white--text mb-2 display-1 text-center">innovators & developers community</h1>
           </v-layout>
         </v-parallax>
       </section>
@@ -30,9 +18,7 @@
           <v-flex xs12 sm4 class="my-4">
             <div class="text-center">
               <h2 class="headline">The best way to start developing</h2>
-              <span class="subheading">
-                Cras facilisis mi vitae nunc
-              </span>
+              <span class="subheading">Cras facilisis mi vitae nunc</span>
             </div>
           </v-flex>
           <v-flex xs12>
@@ -41,9 +27,7 @@
                 <v-flex xs12 md4>
                   <v-card flat class="transparent">
                     <v-card-text class="text-center">
-                      <v-icon x-large class="blue--text text--lighten-2"
-                        >mdi-palette</v-icon
-                      >
+                      <v-icon x-large class="blue--text text--lighten-2">mdi-palette</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
                       <div class="headline text-center">Material Design</div>
@@ -60,9 +44,7 @@
                 <v-flex xs12 md4>
                   <v-card flat class="transparent">
                     <v-card-text class="text-center">
-                      <v-icon x-large class="blue--text text--lighten-2"
-                        >mdi-flash</v-icon
-                      >
+                      <v-icon x-large class="blue--text text--lighten-2">mdi-flash</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
                       <div class="headline">Fast development</div>
@@ -79,14 +61,10 @@
                 <v-flex xs12 md4>
                   <v-card flat class="transparent">
                     <v-card-text class="text-center">
-                      <v-icon x-large class="blue--text text--lighten-2"
-                        >mdi-wrench</v-icon
-                      >
+                      <v-icon x-large class="blue--text text--lighten-2">mdi-wrench</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-center">
-                        Completely Open Sourced
-                      </div>
+                      <div class="headline text-center">Completely Open Sourced</div>
                     </v-card-title>
                     <v-card-text>
                       Cras facilisis mi vitae nunc lobortis pharetra. Nulla
@@ -106,9 +84,7 @@
       <section>
         <v-parallax src="@/assets/section.jpg" height="380">
           <v-layout column align-center justify-center>
-            <div class="headline white--text mb-4 text-center">
-              Web development has never been easier
-            </div>
+            <div class="headline white--text mb-4 text-center">Web development has never been easier</div>
             <em>Kick-start your application today</em>
             <v-btn
               class="mt-12"
@@ -116,9 +92,7 @@
               dark
               large
               href="/pre-made-themes"
-            >
-              Get Started
-            </v-btn>
+            >Get Started</v-btn>
           </v-layout>
         </v-parallax>
       </section>
@@ -152,9 +126,7 @@
                 <v-list class="transparent">
                   <v-list-item>
                     <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2"
-                        >mdi-phone</v-icon
-                      >
+                      <v-icon class="blue--text text--lighten-2">mdi-phone</v-icon>
                     </v-list-item-action>
                     <v-list-item-content>
                       <v-list-item-title>777-867-5309</v-list-item-title>
@@ -162,9 +134,7 @@
                   </v-list-item>
                   <v-list-item>
                     <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2"
-                        >mdi-map-marker</v-icon
-                      >
+                      <v-icon class="blue--text text--lighten-2">mdi-map-marker</v-icon>
                     </v-list-item-action>
                     <v-list-item-content>
                       <v-list-item-title>Chicago, US</v-list-item-title>
@@ -172,9 +142,7 @@
                   </v-list-item>
                   <v-list-item>
                     <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2"
-                        >mdi-email</v-icon
-                      >
+                      <v-icon class="blue--text text--lighten-2">mdi-email</v-icon>
                     </v-list-item-action>
                     <v-list-item-content>
                       <v-list-item-title>john@vuetifyjs.com</v-list-item-title>
@@ -192,18 +160,10 @@
           <v-flex xs12>
             <div class="white--text ml-4">
               Made with
-              <v-icon class="red--text">mdi-heart</v-icon>
-              by
-              <a
-                class="white--text"
-                href="https://vuetifyjs.com"
-                target="_blank"
-                >Vuetify</a
-              >
+              <v-icon class="red--text">mdi-heart</v-icon>by
+              <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
               and
-              <a class="white--text" href="https://github.com/vwxyzjn"
-                >Costa Huang</a
-              >
+              <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
             </div>
           </v-flex>
         </v-layout>
