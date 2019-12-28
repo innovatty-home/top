4f0380383a847c26178e1b051513c8bf882b67a3
Kentaro Uei <e35783@jp.ibm.com>
2019-12-28 14:27:01 +0900

    メニュー作成中

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -46,14 +46,12 @@
                       >
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-center">Material Design</div>
+                      <div class="headline text-center">innovattyの理念</div>
                     </v-card-title>
                     <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla
-                      volutpat tincidunt ornare. Pellentesque habitant morbi
-                      tristique senectus et netus et malesuada fames ac turpis
-                      egestas. Nullam in aliquet odio. Aliquam eu est vitae
-                      tellus bibendum tincidunt. Suspendisse potenti.
+                        I n n o v a t i v eでありたいと思う人が、本当にやりたいことを見つけられ、
+                        本当にやりたいことを実現できるよう、ここにI n n o v a t o rの輪を広げ、
+                        I n n o v a t i o nの芽を育み、I n n o v a t o rが心から自由に輝ける場を実現する
                     </v-card-text>
                   </v-card>
                 </v-flex>
@@ -65,14 +63,35 @@
                       >
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline">Fast development</div>
+                      <div class="headline">innovattyの歩き方</div>
                     </v-card-title>
                     <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla
-                      volutpat tincidunt ornare. Pellentesque habitant morbi
-                      tristique senectus et netus et malesuada fames ac turpis
-                      egestas. Nullam in aliquet odio. Aliquam eu est vitae
-                      tellus bibendum tincidunt. Suspendisse potenti.
+                      innovatty slackに入ったらまずやってみよう！
+                      <br>
+                      Step1) 自己紹介してみよう(#_自己紹介)
+                      <br>
+                      自分の興味ごとなどを自己紹介がてらアピールしちゃってください！
+                      <br>
+                      
+                      
+                      イベント情報を手に入れよう
+                      postide-event-live
+                      innovattyの公式企画の告知などはこちらに投下されます！
+                      こまめにチェックして、楽しそうなものには積極的に参加してみてください！
+                      イベントの実況中継用チャンネル#ide-event-liveなんかも用意されています♪
+                      
+                      つぶやいてみよう
+                      とりあえず雑談
+                      で面白い記事を共有したり、ツイッター感覚でつぶやいてみよう！
+                      
+                      企画してみよう
+                      post とりあえず雑談
+                      こんなことをしたい！仲間を集めたい！
+                      そんなときは企画内容を投稿して仲間を募ってみよう！
+                      
+                      チャンネルを作ってみよう
+                      new-channel-info
+                      作ったチャンネルの情報を共有して新チャンネルに人を集めよう！
                     </v-card-text>
                   </v-card>
                 </v-flex>
@@ -85,15 +104,16 @@
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
                       <div class="headline text-center">
-                        Completely Open Sourced
+                        イベント情報
                       </div>
                     </v-card-title>
                     <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla
-                      volutpat tincidunt ornare. Pellentesque habitant morbi
-                      tristique senectus et netus et malesuada fames ac turpis
-                      egestas. Nullam in aliquet odio. Aliquam eu est vitae
-                      tellus bibendum tincidunt. Suspendisse potenti.
+                      2020年一発目！！
+                      IBM × Panasonic有志によるイノベーション志向勉強会第２弾！
+                      ３月開催予定のハッカソンに向けて、各種テクノロジーの勉強会を実施していきます。
+                      今回のテーマは・・・IBM Watson × チャットボット。
+
+                      ハンズオン形式でお米を炊くくらい簡単にチャットボット作りを体験していただけます。 
                     </v-card-text>
                   </v-card>
                 </v-flex>
