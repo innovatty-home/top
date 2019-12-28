e772cf226c140b975fa2f0055d701f3fb54e929a
Sohei Uchino <suchino@jp.ibm.com>
2019-12-28 15:40:29 +0900

    ContactUsのコンポーネント化

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-flex xs12 sm4 offset-sm1>
+    <v-card flat class="transparent">
+      <v-card-title primary-title class="layout justify-center">
+        <div class="headline">Contact us</div>
+      </v-card-title>
+      <v-card-text>
+        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt
+        ornare.
+      </v-card-text>
+      <v-list class="transparent">
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon class="blue--text text--lighten-2">mdi-phone</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>777-867-5309</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon class="blue--text text--lighten-2">mdi-map-marker</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Chicago, US</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon class="blue--text text--lighten-2">mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>john@vuetifyjs.com</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-flex>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -58,49 +58,7 @@
         <v-container grid-list-xl>
           <v-layout row wrap justify-center class="my-12">
             <AboutUs />
-            <v-flex xs12 sm4 offset-sm1>
-              <v-card flat class="transparent">
-                <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Contact us</div>
-                </v-card-title>
-                <v-card-text>
-                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat
-                  tincidunt ornare.
-                </v-card-text>
-                <v-list class="transparent">
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2"
-                        >mdi-phone</v-icon
-                      >
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>777-867-5309</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2"
-                        >mdi-map-marker</v-icon
-                      >
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>Chicago, US</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2"
-                        >mdi-email</v-icon
-                      >
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>john@vuetifyjs.com</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card>
-            </v-flex>
+            <ContactUs />
           </v-layout>
         </v-container>
       </section>
@@ -133,10 +91,11 @@
 import Vue from 'vue'
 import AboutUs from '@/components/AboutUs.vue'
 import TopMenu from '@/components/TopMenu.vue'
+import ContactUs from '@/components/ContactUs.vue'
 
 export default Vue.extend({
   name: 'HelloWorld',
-  components: { AboutUs, TopMenu },
+  components: { AboutUs, TopMenu, ContactUs },
 
   data: () => ({})
 })
