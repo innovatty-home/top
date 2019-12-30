2774b231c4446f4b57fc2f10df9ad378cc378863
Sohei Uchino <suchino@jp.ibm.com>
2019-12-30 20:32:47 +0900

    メンバーリストにclickableアイコン表示

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -5,19 +5,32 @@
         <div class="headline">About us</div>
       </v-card-title>
       <v-flex xs12 sm12>
-        <v-card flat max-width="650" max-height="250" class="mx-auto" style="overflow-y: scroll">
+        <v-card
+          flat
+          max-width="650"
+          max-height="250"
+          class="mx-auto"
+          style="overflow-y: scroll"
+        >
           <v-list>
             <v-list-item v-for="item in this.memberList" :key="item.id">
               <v-dialog width="500">
                 <template v-slot:activator="{ on }">
                   <v-layout raw v-on="on" style="margin-top:6px">
                     <v-list-item-avatar>
-                      <v-img :src="require('@/assets/profile/' + item.id + '.png')"></v-img>
+                      <v-img
+                        :src="require('@/assets/profile/' + item.id + '.png')"
+                      ></v-img>
                     </v-list-item-avatar>
 
                     <v-list-item-content>
-                      <v-list-item-title v-text="item['氏名']"></v-list-item-title>
+                      <v-list-item-title
+                        v-text="item['氏名']"
+                      ></v-list-item-title>
                     </v-list-item-content>
+                    <v-list-item-icon style="margin-right:10px">
+                      <v-icon>mdi-open-in-new</v-icon>
+                    </v-list-item-icon>
                   </v-layout>
                 </template>
                 <ProfileDetail :profile="item" />
