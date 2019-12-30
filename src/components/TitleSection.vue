e7f650e8b54a8b45823fe8424858a2d1159efe91
Sohei Uchino <suchino@jp.ibm.com>
2019-12-30 15:41:21 +0900

    タイトル画像の大きさと文字サイズをレスポンシブに

--- src/components/TitleSection.vue
+++ src/components/TitleSection.vue
@@ -2,11 +2,22 @@
   <section>
     <v-parallax src="@/assets/bg1.png" height="500">
       <v-layout column align-center justify-center class="white--text">
-        <img src="@/assets/innovatty_logo.png" alt="Vuetify.js" height="200" />
-        <h1 class="white--text mb-2 display-1 text-center">
+        <img
+          id="title_image"
+          src="@/assets/innovatty_logo.png"
+          alt="Vuetify.js"
+          width="80%"
+        />
+        <h1 id="title_text" class="white--text font-weight-light text-center">
           innovators & developers community
         </h1>
       </v-layout>
     </v-parallax>
   </section>
 </template>
+
+<style lang="scss" scoped>
+#title_text {
+  font-size: 3vw;
+}
+</style>
