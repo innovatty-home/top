4d01a8a8c9b1d47e4d6ec6686b7994ac2cc6e3cc
Sohei Uchino <suchino@jp.ibm.com>
2019-12-30 15:44:11 +0900

    meetup画像のところをSubSection.vueとしてコンポーネント切り出し

---
+++ src/components/SubSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <v-parallax src="@/assets/meetup_photo.png" height="380">
+      <v-layout column align-center justify-center>
+        <div class="headline white--text mb-4 text-center">
+          Already a member?
+        </div>
+        <em>If so, let's talk in Slack right away</em>
+        <v-btn
+          class="mt-12"
+          color="blue lighten-2"
+          dark
+          large
+          href="https://innovatty.slack.com/app_redirect?channel=_post"
+          target="_blank"
+          >GET START</v-btn
+        >
+      </v-layout>
+    </v-parallax>
+  </section>
+</template>
--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -20,27 +20,7 @@
           </v-flex>
         </v-layout>
       </section>
-
-      <section>
-        <v-parallax src="@/assets/meetup_photo.png" height="380">
-          <v-layout column align-center justify-center>
-            <div class="headline white--text mb-4 text-center">
-              Already a member?
-            </div>
-            <em>If so, let's talk in Slack right away</em>
-            <v-btn
-              class="mt-12"
-              color="blue lighten-2"
-              dark
-              large
-              href="https://innovatty.slack.com/app_redirect?channel=_post"
-              target="_blank"
-              >GET START</v-btn
-            >
-          </v-layout>
-        </v-parallax>
-      </section>
-
+      <SubSection />
       <section>
         <v-container grid-list-xl>
           <v-layout row wrap justify-center class="my-12">
@@ -59,11 +39,12 @@ import AboutUs from '@/components/AboutUs.vue'
 import TopMenu from '@/components/TopMenu.vue'
 import ContactUs from '@/components/ContactUs.vue'
 import TitleSection from '@/components/TitleSection.vue'
+import SubSection from '@/components/SubSection.vue'
 // import axios from 'axios'
 
 export default Vue.extend({
   name: 'TopPage',
-  components: { AboutUs, TopMenu, ContactUs, TitleSection },
+  components: { AboutUs, TopMenu, ContactUs, TitleSection, SubSection },
 
   data: () => ({
     hoge: ''
