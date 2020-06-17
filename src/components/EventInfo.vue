91642deb9d5b9a52462cc545ceef83961161c64f
SOHEI UCHINO <SUCHINO@jp.ibm.com>
2020-06-17 21:21:51 +0900

    イベント詳細ページがなければボタンを表示しない

--- src/components/EventInfo.vue
+++ src/components/EventInfo.vue
@@ -1,34 +1,58 @@
 <template>
   <v-layout row wrap class="px-3">
-    <v-flex xs12 sm12 md6 lg4 xl3 v-for="(eventObj,index) in eventsArr" :key=index class="mb-4">    
-      <v-card class="mx-auto" max-width="400">
-        <v-img class="white--text align-end" height="200px" :src=eventObj.イベント画像URL>
-          <v-card-title class="subtitle-1 card-titile-background">{{eventObj.イベント名}}</v-card-title>
+    <v-flex
+      xs12
+      sm12
+      md6
+      lg4
+      xl3
+      v-for="(eventObj, index) in eventsArr"
+      :key="index"
+      class="mb-4"
+    >
+      <v-card class="mx-auto" max-width="450" height="450">
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          :src="eventObj.イベント画像URL"
+        >
+          <v-card-title class="subtitle-1 card-titile-background">{{
+            eventObj.イベント名
+          }}</v-card-title>
         </v-img>
-        <v-card-subtitle class="pb-0">{{eventObj.イベント開催日時}}@{{eventObj.イベント開催場所}}</v-card-subtitle>
+        <v-card-subtitle class="pb-0"
+          >{{ eventObj.イベント開催日時 }}@{{
+            eventObj.イベント開催場所
+          }}</v-card-subtitle
+        >
         <div class="text-start">
           <v-chip class="ma-2" small color="primary">
-          {{eventObj.イベント種類}}
+            {{ eventObj.イベント種類 }}
           </v-chip>
-          <v-chip class="ma-2" small :color="(eventObj.isEnded)?'default':'primary'">
-          {{eventObj.isEnded === true?'イベント終了':'参加者募集中'}}
-          </v-chip>          
-        </div>        
-        <v-card-text class="text--primary">
-          <div>{{eventObj.イベント詳細}}</div>
+          <v-chip
+            class="ma-2"
+            small
+            :color="eventObj.isEnded ? 'default' : 'primary'"
+          >
+            {{ eventObj.isEnded === true ? 'イベント終了' : '参加者募集中' }}
+          </v-chip>
+        </div>
+        <v-card-text class="text--primary" style="padding-bottom: 0px">
+          <div>{{ eventObj.イベント詳細 }}</div>
         </v-card-text>
         <v-card-actions>
           <v-btn
+            v-if="eventObj.イベントURL"
             color="orange"
             text
-            :href=eventObj.イベントURL
+            :href="eventObj.イベントURL"
             target="_blank'"
           >
             イベント開催ページへ
           </v-btn>
         </v-card-actions>
-      </v-card> 
-    </v-flex>   
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 
@@ -40,20 +64,24 @@ const today = new Date()
 
 // イベントが終了しているか否かを返す
 const isEndedEvent = (eventDate: String): Boolean => {
-    let dateArr:String[] = eventDate.split(/\/|\(/, 3)
-    let tmpEventDate = new Date(Number(dateArr[0]), Number(dateArr[1]) - 1, Number(dateArr[2]), 0, 0)
+  let dateArr: String[] = eventDate.split(/\/|\(/, 3)
+  let tmpEventDate = new Date(
+    Number(dateArr[0]),
+    Number(dateArr[1]) - 1,
+    Number(dateArr[2]),
+    0,
+    0
+  )
 
-    if (today > tmpEventDate){      
-      return true
-    }else{
-      return false
-    }    
+  if (today > tmpEventDate) {
+    return true
+  } else {
+    return false
+  }
 }
 
 @Component
 export default class EventInfo extends Vue {
-
-
   eventsArr: any = []
 
   created() {
@@ -62,19 +90,20 @@ export default class EventInfo extends Vue {
       .then(response => {
         // スプレッドシートは下の行に最新データが追加されるので、配列の中身を反転して、最新の情報がindexの上に来るようにする
         this.eventsArr = response.data.reverse()
-        this.eventsArr.map((eventObj:any) => eventObj.isEnded = isEndedEvent(eventObj.イベント開催日時))
-
+        this.eventsArr.map(
+          (eventObj: any) =>
+            (eventObj.isEnded = isEndedEvent(eventObj.イベント開催日時))
+        )
       })
       .catch(reason => {
         console.log(reason)
       })
   }
 }
-
 </script>
 
 <style lang="scss" scoped>
-.card-titile-background{
-  background-color: rgba(0, 0, 0, 0.6)
+.card-titile-background {
+  background-color: rgba(0, 0, 0, 0.6);
 }
 </style>
